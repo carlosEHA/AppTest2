4e18519b882056ce958822b031c80fe3f9f98e72
Carlos Eduardo Hernandez Aguilar <carloseduardohernandezaguilar@MacBook-Pro-de-Carlos.local>
2018-05-07 20:44:38 -0500

    Terminado

--- AppTest2/ViewController.swift
+++ AppTest2/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        let startDate = formatter.date(from: "2010/06/11")
+        let finishDate = formatter.date(from: "2012/06/11")
+        self.appTest2(start: startDate!, end: finishDate!)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    func appTest2(start: Date,end :Date){
+        
+        let calendar = Calendar.current
+        var date = start
+        let endDate = end
+        
+        print("Inicio: \(date) final: \(endDate)")
+        
+        let fmt = DateFormatter()
+        fmt.dateFormat = "dd/MM/yyyy"
+        var count = 0
+        while date <= endDate {
+            
+            date = calendar.date(byAdding: .day, value: 1, to: date)!
+            
+            let day = calendar.component(.day, from: date)
+            let formatter = DateFormatter()
+            formatter.dateFormat = "EEEE"
+            let dayInWeek = formatter.string(from: date)
+            print(day)
+            if(dayInWeek == "Sunday" && day == 1){
+                count = count + 1
+            }
+        }
+        print("Domingos: \(count)")
+    }
 }
 
